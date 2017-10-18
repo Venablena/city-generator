8e4c5d0cfe6fd6856391dc057705d8f7369890a2
Lena Venable <venablena@gmail.com>
2017-10-18 10:07:12 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const faker = require('faker')
+
+function createCities(num){
+  //returns an array of city objects
+  const result = []
+  for (let i = 0; i < num; i++) {
+    const name = faker.address.city()
+    const population = faker.random.number() * 10
+    const country = faker.address.country()
+    const zipCode = faker.address.zipCode()
+    const motto = faker.lorem.sentence()
+    const city = {}
+    city[name] = {population, country, zipCode, motto}
+    result.push(city)
+  }
+  return result
+}
