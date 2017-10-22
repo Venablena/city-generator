91d4a397f66da644cca7d90dc9a8473c7fe5a6e7
Lena Venable <venablena@gmail.com>
2017-10-22 16:30:41 -0700

    add unique id

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ function createCities(num){
   const result = []
   for (let i = 0; i < num; i++) {
     const name = faker.address.city()
+    const id = faker.random.uuid()
     const population = faker.random.number() * 10
     const country = faker.address.country()
     const zipCode = faker.address.zipCode()
     const motto = faker.lorem.sentence()
     const city = {}
-    city[name] = {population, country, zipCode, motto}
+    city[name] = {population, country, zipCode, motto, id}
     result.push(city)
   }
   return result
